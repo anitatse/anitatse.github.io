28b1c28605110ec6156147e8d728bbd74c21253e
anitatse <anita-tse@hotmail.com>
2019-08-09 19:51:54 -0700

    added mobile nav menu

--- js/website.js
+++ js/website.js
@@ -1,8 +1,8 @@
 // taken from https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("topnav");
+function mobileNavBar() {
+  var x = document.getElementById("navitems");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
