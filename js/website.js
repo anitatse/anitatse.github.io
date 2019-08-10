07bdd3244623525c65a389cf7de1bcf073b24b79
anitatse <anita-tse@hotmail.com>
2019-08-09 18:52:30 -0700

    trying mobile navbar

---
+++ js/website.js
@@ -0,0 +1,11 @@
+// taken from https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
