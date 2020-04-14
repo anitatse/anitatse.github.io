1d0c8584b51119a437ea68af88257c79694b0ad7
anitatse <anita-tse@hotmail.com>
2020-04-14 15:33:04 -0700

    removed jquery

--- js/website.js
+++ js/website.js
@@ -1,10 +1,5 @@
 // taken from https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 
-
-$(function() {
-    $('body').removeClass('fade-out');
-});
-
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function mobileNavBar() {
   var x = document.getElementById("navitems");
